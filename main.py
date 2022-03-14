91d16e3f57c3d3ef84f06cbd891a799a9ed2bf77
mm <mjmichaelson@gmail.com>
2022-03-14 16:10:42 -0400

    finished part 2 in 2 different ways

--- main.py
+++ main.py
@@ -14,11 +14,26 @@ def main():
         # print(f'{im.format}, {im.size}, {im.mode}')
         print(f'Image size: {im.size[0]} x {im.size[1]}')
 
+    # load pixels into matrix
     try:
-        pixel_matrix = np.asarray(im)
+        # by-hand method
+        data_list = [im.getdata()]
+        pixel_matrix = [[] for row in range(im.size[0])]
+        i = 0
+        for r in range(im.size[0]):
+            for c in range(im.size[1]):
+                pixel_matrix[r].append(data_list[0][i])
+                i += 1
+
+        # numpy reshape method
+        d = np.reshape(np.array(im.getdata()), (im.size[0], im.size[1], 3))
+        #print(d)
+        #print(d.size)
 
         # Iterate though all pixels to test
-        print(pixel_matrix)
+        #print(pixel_matrix)
+        #print(len(pixel_matrix), len(pixel_matrix[0]))
+        
     except:
         print(f'Oops! Attempting to construct '
               f'pixel matrix but {sys.exc_info()[0]} occurred.')
