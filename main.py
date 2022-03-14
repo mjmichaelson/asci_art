33b9d2fd8a2de946e18ff1a89abb144313324369
mm <mjmichaelson@gmail.com>
2022-03-14 17:50:46 -0400

    finished but still image is too large

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ import sys
 from PIL import Image
 import numpy as np
 
+_ASCII_BRIGHTNESS_MAP = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
 
 def convert_rgb_pixel_to_brightness(p):
     # average method
     return (p[0] + p[1] + p[2]) // 3
+
+
+def convert_brightness_to_ascii(b):
+    # scale factor should be slightly higher than the actual divisor, to ensure no
+    # resulting numbers are too large
+    scale_factor = round((255 / len(_ASCII_BRIGHTNESS_MAP) + 0.01), 2)
+    return int(b // scale_factor)
+
+
+def print_ascii_matrix(mat):
+    for i in mat:
+        for j in i:
+            # print 3 times to counteract 'squashing' that results from
+            # conversion of a square pixel to a rectangular ascii char
+            print(j, sep='', end='')
+            print(j, sep='', end='')
+            print(j, sep='', end='')
+        print('/n')
+
 
 def main():
     # PART 1: load the image
@@ -63,6 +84,22 @@ def main():
     else:
         print('Brightness matrix constructed successfully!')
         print(f'size: {len(brightness_matrix)} x {len(brightness_matrix[0])}')
+
+    # PART 4: Construct ASCII Matrix
+    try:
+        ascii_matrix = [[] for r in range(len(brightness_matrix))]
+        for i in range(len(brightness_matrix)):
+            for j in brightness_matrix[i]:
+                a = convert_brightness_to_ascii(j)
+                ascii_pixel = _ASCII_BRIGHTNESS_MAP[a]
+                ascii_matrix[i].append(ascii_pixel)
+    except:
+        print(f'Oops! Attempting to construct '
+              f'ASCII matrix but {sys.exc_info()[0]} occurred.')
+    else:
+        print(f'Success! Made an ASCII matrix!')
+        print(ascii_matrix)
+        print_ascii_matrix(ascii_matrix)
 
 
 if __name__ == '__main__':
